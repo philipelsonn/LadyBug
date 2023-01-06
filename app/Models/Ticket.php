dfda9fc080ec0ec67c45ad39b726c060eb3f62aa
philipelsonn <83831775+philipelsonn@users.noreply.github.com>
2023-01-06 13:00:06 +0700

    first commit

---
+++ app/Models/Ticket.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    use HasFactory;
+    protected $table = 'tickets';
+    protected $primaryKey = 'id';
+    protected $timestamp = true;
+    protected $guarded = [];
+}
---
+++ database/migrations/2023_01_06_054600_create_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->unsignedBigInteger('ticket_id')->references('id')->on('ticket')->onUpdate('cascade')->onDelete('cascade');
+            $table->longText('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
