745673b1805643f96508db6f4f073d45b5fb3aec
philipelsonn <83831775+philipelsonn@users.noreply.github.com>
2023-01-19 00:58:25 +0700

    add: email notification

---
+++ resources/views/emails/staff.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>New Ticket</title>
+</head>
+<body>
+ 
+ <h1>Notification from LadyBug</h1>
+ <p>There is a new ticket assigned for you. Check it now!</p>
+ 
+</body>
+</html> 
---
+++ resources/views/emails/user.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Resolved</title>
+</head>
+<body>
+ 
+ <h1>Notification from LadyBug</h1>
+ <p>Your submission has been resolved! Thank you for reporting!</p>
+ 
+</body>
+</html> 
