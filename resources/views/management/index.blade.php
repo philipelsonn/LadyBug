b22a7cf981308659674a1361e9e4fa037daab44f
Hans <hansnathanaeljr@gmail.com>
2023-01-19 00:48:42 +0700

    submission index + create

--- resources/views/management/index.blade.php
+++ resources/views/management/index.blade.php
@@ -9,48 +9,50 @@
         </div>
     @else
         <h2 class="fw-bold text-center mt-4">Manage User</h2>
-        <div class="container mt-2">
-            <table class="table table bg-light">
-                <thead>
-                    <tr class="text-center">
-                        <th class="col-md-1 align-middle">No</th>
-                        <th class="col-md-1 align-middle"></th>
-                        <th class="col-md-3 align-middle">Name</th>
-                        <th class="col-md-3 align-middle">Email</th>
-                        <th class="col-md-2 align-middle">Type</th>
-                        <th class="col-md-2 align-middle">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @php($i = 1)
-                    @foreach ($users as $user)
-                        <tr class="text-center">
-                            <td class="align-middle fw-bold">{{ $i }}</td>
-                            <td class="align-middle">
-                                <img src="{{ $user->image }}" alt="" class="rounded-circle" style="width:40px; height: 40px">
-                            </td>
-                            <td class="align-middle">
-                                {{ $user->name }}
-                            </td>
-                            <td class="align-middle">{{ $user->email }}</td>
-                            <td class="align-middle">{{ $user->type }}</td>
-                            <td class="align-middle">
-                                <div class="d-flex justify-content-center">
-                                    <a class="btn btn-sm btn-warning text-dark fw-bold me-2" href="/updateUser/{{ $user->id }}">UPDATE</a>
-                                    <form action="/deleteUser/{{ $user->id }}" method="POST">
-                                        @csrf
-                                        <button type="submit" class="btn btn-sm btn-block btn-danger text-white fw-bold"
-                                        onclick="return confirm('Are you sure you want to permanently delete the data?')">
-                                        DELETE</button>
-                                    </form>
-                                </div>
-                            </td>
-                            </td>
+        <div class="container mt-4">
+            <div class="card p-4 bg-light">
+                <table id="myTable" class="table table-striped bg-light">
+                    <thead>
+                        <tr class="">
+                            <th class="col-md-1 align-middle">No</th>
+                            <th class="col-md-1 align-middle"></th>
+                            <th class="col-md-3 align-middle">Name</th>
+                            <th class="col-md-3 align-middle">Email</th>
+                            <th class="col-md-2 align-middle">Type</th>
+                            <th class="col-md-2 align-middle">Action</th>
                         </tr>
-                        @php($i = $i + 1)
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        @php($i = 1)
+                        @foreach ($users as $user)
+                            <tr class="">
+                                <td class="align-middle fw-bold">{{ $i }}</td>
+                                <td class="align-middle">
+                                    <img src="{{ $user->image }}" alt="" class="rounded-circle" style="width:40px; height: 40px">
+                                </td>
+                                <td class="align-middle">
+                                    {{ $user->name }}
+                                </td>
+                                <td class="align-middle">{{ $user->email }}</td>
+                                <td class="align-middle">{{ $user->type }}</td>
+                                <td class="align-middle">
+                                    <div class="d-flex justify-content-center">
+                                        <a class="btn btn-sm btn-warning text-dark fw-bold me-2" href="/updateUser/{{ $user->id }}">UPDATE</a>
+                                        <form action="/deleteUser/{{ $user->id }}" method="POST">
+                                            @csrf
+                                            <button type="submit" class="btn btn-sm btn-block btn-danger text-white fw-bold"
+                                            onclick="return confirm('Are you sure you want to permanently delete the data?')">
+                                            DELETE</button>
+                                        </form>
+                                    </div>
+                                </td>
+                                </td>
+                            </tr>
+                            @php($i = $i + 1)
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     @endif
 @endsection
--- resources/views/submissions/create.blade.php
+++ resources/views/submissions/create.blade.php
@@ -3,227 +3,76 @@
 @section('title', 'LadyBug | Create Submission')
 
 @section('content')
-{{-- <style>
-  body {
-    font-family: "Open Sans", sans-serif;
-  }
-  
-  .container {
-    max-width: 800px;
-    margin: 0 auto;
-    padding: 0 20px;
-  }
-  
-  .card {
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-    border-radius: 10px;
-    background: #fff;
-  }
-  
-  .card-shadow {
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  }
-  
-  .rounded-20 {
-    border-radius: 20px;
-  }
-  
-  .title-line {
-  border-bottom: 1px solid #ccc;
-  width: 100px;
-  margin: 0 auto 20px;
-  }
-
-  .subheading-text {
-    font-size: 18px;
-    margin-bottom: 10px;
-  }
-
-  .c-text-1 {
-    font-size: 20px;
-    color: #333;
-  }
-  
-  .fw-bold {
-    font-weight: bold;
-    font-size: 24px;
-  }
-  
-  .form-select {
-  appearance: none;
-  border: none;
-  background: #fafafa;
-  border-radius: 20px;
-  font-size: 16px;
-  padding: 10px 20px;
-  width: 250px;
-  }
-  
-  .form-control {
-    border-radius: 20px;
-    border: 1px solid #ccc;
-    font-size: 16px;
-    padding: 20px;
-    width: 100%;
-  }
-  
-  .btn-outline-1 {
-  border: 0.5px solid #333;
-  border-radius: 10px;
-  color: #333;
-  font-size: 14px;
-  padding: 5px 10px;
-  text-transform: uppercase;
-  transition: all 0.3s ease;
-  }
-  
-  .btn-outline-1:hover {
-    background: #333;
-    color: #fff;
-  }
-  
-  .form-group {
-      display: flex;
-      align-items: center;
-      padding: 5px;
-      margin-bottom: 5px;
-      padding-left: 20px;
-      padding-bottom: 10px;
-  }
-  
-  .form-group label {
-    width: 120px;
-    font-weight: bold;
-    font-size: 16px;
-  }
-  
-  .form-group .form-select,
-  .form-group .form-control {
-    flex: 1;
-  }
-  
-  .subheading-text {
-    font-size: 18px;
-    padding-left: 20px;
-  }
-  
-  .c-text-1 {
-    font-size: 20px;
-    padding-left: 20px;
-  }
-  
-  textarea.form-control {
-    height: 100%;
-    width: 100%;
-  }
-  
-  .container {
-  background: #f5f5f5;
-  }
-
-  .card {
-    background: #ffffff;
-  }
-  
-  .c-text-1 {
-    color: #1e90ff;
-  }
-
-  .form-select {
-    border-color: #1e90ff;
-  }
-
-  .btn-outline-1 {
-    background: #1e90ff;
-    color: #fff;
-    border-color: #1e90ff;
-  }
-
-  .btn-outline-1:hover {
-    background: #fff;
-    color: #1e90ff;
-  }
-</style> --}}
-
-<div class="container mt-5 py-5">
-    <div class=" card card-shadow border-0 rounded-20 ">
-        <div class="card-body my-3">
-            <div class="title-line"></div> 
-            <h5 class="subheading-text mt-3">Submissions</h5>
-            <h3 class="fw-bold my-3 c-text-1">Create New Submission</h3>
-            <hr>
-            <form method="POST" action="{{route('submissions.store')}}" enctype="multipart/form-data">
-                @csrf
-                <div class="row">
-                    <div class="col-12 col-md-12">
-                        <div class="form-group row mb-0 mb-sm-3">
-                            <label for="type" class="col-sm-3 col-form-label text-sm-left">
-                                Type</label>
-                            <div class="col-sm-9">
-                                <select name="type" id="type" class="form-select rounded-pill"
-                                    placeholder="">
-                                    <option value="" selected disabled>Choose...
-                                    </option>
-                                    <option value="FEEDBACK">
-                                        Feedback
-                                    </option>
-                                    <option value="BUG">
-                                        Bug/Error/Issue
-                                    </option>
-                                </select>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0 mb-sm-3">
-                            <label for="topic" class="col-sm-3 col-form-label text-sm-left">
-                                Topic</label>
-                            <div class="col-sm-9">
-                                <select name="topic" id="topic" class="form-select rounded-pill" 
-                                    placeholder="" >
-                                    <option value="" selected disabled>Choose...
-                                    </option>
-                                    @foreach ($topics as $topic)
-                                    <option value="{{$topic->title}}">
-                                        {{$topic->title}}
-                                    </option>
-                                    @endforeach
-                                </select>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0 mb-sm-3">
-                          <label for="title" class="col-sm-3 col-form-label text-sm-left">
-                              Title</label>
-                          <div class="col-sm-9">
-                              <input type="text" id="title" name="title" class="form-control rounded-pill" required>
-                          </div>
-                        </div>
-
-                        <div class="form-group row mb-0 mb-sm-3">
-                            <label for="description" class="col-sm-3 col-form-label text-sm-left">
-                                Description</label>
-                            <div class="col-sm-9">
-                                <textarea rows="5" type="text" id="description" name="description" class="form-control placeholder="Enter Item Description" required>{{ old('description') }}</textarea>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0 mb-sm-3">
-                            <label for="image" class="col-sm-3 col-form-label text-sm-left">
-                                Attach a screenshot</label>
-                            <div class="col-md-12">
-                                <input class="form-control" type="file" id="image" name="image">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mt-5">
-                            <div class="d-grid gap-2">
-                                <button type="submit" class="btn btn-outline-1 rounded-20">Submit</button>
-                            </div>
-                        </div>
-                    </div>
+    <h2 class="fw-bold text-center mt-4">Create Submission</h2>
+    <div class="container mt-4">
+        <form action="{{ route('submissions.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="row justify-content-center">
+                <div class="col-md-4">
+                    <label for="type" class="col-md-4 col-form-label text-sm-left fw-bold">
+                        {{_('Submission Type')}}</label>
+                    <select name="type" class="form-select @error('type')
+                        is-invalid
+                    @enderror" required>
+                        <option value="Select" selected disabled>Select Submission Type</option>
+                        <option value="Feedback" @if (old('type') == 'Feedback')
+                            selected
+                        @endif>Feedback</option>
+                        <option value="Bug/Error" @if (old('type') == 'Bug/Error')
+                            selected
+                        @endif>Bug/Error</option>
+                    </select>
                 </div>
-            </form>
-        </div>
+                <div class="col-md-4">
+                    <label for="type" class="col-md-4 col-form-label text-sm-left fw-bold">
+                        {{_('Submission Topic')}}</label>
+                    <select name="topic" class="form-select @error('topic')
+                        is-invalid
+                    @enderror" required>
+                        <option value="Select" selected disabled>Select Submission Topic</option>
+                        @foreach ($topics as $topic)
+                            <option value="{{ $topic->title }}" @if (old('topic') == $topic->title)
+                                selected
+                            @endif>{{ $topic->title }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+            <div class="col-md-8 form-floating mx-auto mt-3">
+                <input type="text" id="title" name="title" class="form-control @error('title')
+                    is-invalid
+                @enderror" placeholder="Submission Title" value="{{ old('title') }}" required>
+                <label for="title">Submission Title</label>
+                @error('title')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+                @enderror
+            </div>
+            <div class="col-md-8 form-floating mx-auto mt-3">
+                <textarea rows="3" type="text" id="description" name="description" class="form-control @error('description')
+                    is-invalid
+                @enderror" placeholder="Description" required>{{ old('description') }}</textarea>
+                <label for="description">Description</label>
+                @error('description')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+                @enderror
+            </div>
+            <div class="col-md-8 mx-auto mt-3">
+                <input type="file" id="image" name="image" class="form-control @error('image')
+                    is-invalid
+                @enderror">
+                @error('image')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+                @enderror
+            </div>
+            <div class="d-flex justify-content-center mt-3">
+                <button type="submit" class="btn rounded-20 bg-warning fw-bold">Create</button>
+            </div>
+        </form>
     </div>
-</div>
 @endsection
